7188a268139d3ee740e38c0ad9d369f2a3aafc23
Clara Charriau <clara.charriau@renault.com>
2023-04-27 17:45:02 +0200

    [SECURITY] starting security configuration

---
+++ src/main/java/com/project/todoapp/config/securityConfig.java
@@ -0,0 +1,21 @@
+package com.project.todoapp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableWebSecurity
+public class securityConfig {
+
+    //@Bean
+    //public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       // http.authorizeRequests()
+        //        .anyRequest()
+             //   .authenticated()
+             //   .and()
+              //  .csrf().disable()
+              //  .httpBasic();
+      // return http.build();
+    //}
+
+
+}
