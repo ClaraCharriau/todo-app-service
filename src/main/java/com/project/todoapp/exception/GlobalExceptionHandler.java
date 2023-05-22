cf408a32812348ca9026ac75c0c313c613c2c899
Clara Charriau <clara.charriau@renault.com>
2023-05-22 16:38:32 +0200

    [SECURITY] implementing OAuth2.0 with GitHub

--- src/main/java/com/project/todoapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/todoapp/exception/GlobalExceptionHandler.java
@@ -4,15 +4,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * Handle exceptions for this service.
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
